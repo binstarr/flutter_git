4c56840e38b7d1f2873e151dff6e740aaca59e1a
binstarr <tjdqls2s@naver.com>
2022-11-25 17:44:06 +0900

    카카오완료

--- kakao_01/lib/screens/more_screen.dart
+++ kakao_01/lib/screens/more_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_01/models/tab.dart';
 
 class MoreScreen extends StatelessWidget {
   const MoreScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("morescreen"),);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("더보기"),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 30,),
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 4,
+              children: List.generate(tabs.length, (index) => Column(
+                children: [
+                  Icon(tabs[index].icon),
+                  SizedBox(height: 5,),
+                  Text(tabs[index].text)
+                ],
+              )),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
